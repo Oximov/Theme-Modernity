89b7edcccc5bb05f48068a2a6a76d0105997eae1
Oximov <53126647+Oximov@users.noreply.github.com>
2020-05-28 09:30:00 +0200

    Add files via upload

--- views/maintenance.blade.php
+++ views/maintenance.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
     <div class="container content-parent">
-        <h1 class="title-block">{{ trans('messages.maintenance') }}</h1>
+        <h1 class="title-block page-title">{{ trans('messages.maintenance') }}</h1>
 
         <div class="content">
-            <h1>{!! setting('maintenance-message', 'The website is under maintenance') !!}</h1>
+            <h1 style="text-align: center;">{!! setting('maintenance-message', 'The website is under maintenance') !!}</h1>
         </div>
     </div>
 @endsection
